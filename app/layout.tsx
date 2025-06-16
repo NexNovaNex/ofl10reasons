3486ec04dc65fa7c1def00a1f3464d753fb65f59
Thomas Roy <thomasroy@Thomass-Mac-mini.local>
2025-06-16 19:54:06 +0800

    Update Facebook Pixel: add second pixel (746886027994806) and update Add to Cart trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '703037149317119');
+            fbq('init', '746886027994806');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -87,6 +88,7 @@ export default function RootLayout({
         {/* Facebook Pixel NoScript Fallback */}
         <noscript>
           <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=703037149317119&ev=PageView&noscript=1" />
+          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=746886027994806&ev=PageView&noscript=1" />
         </noscript>
         {/* End Facebook Pixel NoScript */}
         {children}
